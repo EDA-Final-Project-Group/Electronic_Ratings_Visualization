86c818fa546aabe06afc8e95089a94d547bfeed7
Zhongling Jiang <zj2249@columbia.edu>
2018-12-08 16:03:33 -0500

    update layout

--- app.R
+++ app.R
@@ -8,7 +8,12 @@ ui <- fluidPage(
       selectInput(inputId = "product", label = "Select a Brand", choices = amazon_electronics$name)
     ),
     mainPanel(
-      plotOutput(outputId = "pie")
+      fluidRow(column(width = 12, plotOutput(outputId = "pie4"))),
+      fluidRow(
+        column(width = 4, plotOutput(outputId = "pie")), 
+        column(width = 4, plotOutput(outputId = "pie2")),
+        column(width = 4, plotOutput(outputId = "pie3"))
+      )
     )
   )
 )
@@ -17,26 +22,45 @@ server <- function(input, output, session) {
   reactive_data <- reactive({
     product_recommend <- amazon_electronics[amazon_electronics$name == input$product, ]
     # if pie chart, need the following line
-    #product_recommend <- amazon_electronics[amazon_electronics$name == input$product, 'reviews.doRecommend']
-    #product_recommend[is.na(product_recommend)] <- "NA"
+    product_recommend <- amazon_electronics[amazon_electronics$name == input$product, 'reviews.doRecommend']
+    product_recommend[is.na(product_recommend)] <- "NA"
     return (product_recommend)
   })
   
   output$pie <- renderPlot({
     d <- reactive_data()
     # pie chart
-    #pie(table(d, exclude = NULL))
+    pie(table(d, exclude = NULL), col = c("pink", "grey", "light green"))
     # bar chart
-    ggplot(d, aes(x=factor(reviews.doRecommend)))+
-      geom_bar(width=0.5, fill="steelblue")+
-      theme_minimal()
+    #ggplot(d, aes(x=factor(reviews.doRecommend)))+
+    #  geom_bar(width=0.5, fill="steelblue")+
+    #  theme_minimal()
+  })
+  
+  output$pie2 <- renderPlot({
+    d <- reactive_data()
+    # pie chart
+    pie(table(d, exclude = NULL), col = c("pink", "grey", "light green"))
+  })
+  
+  output$pie3 <- renderPlot({
+    d <- reactive_data()
+    # pie chart
+    pie(table(d, exclude = NULL), col = c("pink", "grey", "light green"))
+  })
+  
+  output$pie4 <- renderPlot({
+    d <- reactive_data()
+    # pie chart
+    pie(table(d, exclude = NULL), col = c("pink", "grey", "light green"))
+    # bar chart
+    #ggplot(d, aes(x=factor(reviews.doRecommend)))+
+    #  geom_bar(width=0.5, fill="steelblue")+
+    #  theme_minimal()
   })
 }
 
 shinyApp(ui, server)
 
-#for (name in unique(amazon_electronics$name)) {
-#  print(name)
-#  print(table(amazon_electronics[amazon_electronics$name == name, 'reviews.doRecommend']))
-#}
+
 
